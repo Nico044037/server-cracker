de8b4a1d715d9116b6b577aa8f32afb701db7162
Nico044037 <nico.koppensteiner0@gmail.com>
2026-02-25 20:15:33 +0100

    Update finder.py

--- finder.py
+++ finder.py
@@ -4,7 +4,7 @@ import time
 import requests
 from mcstatus import JavaServer
 
-API_URL = "https://web-production-79e19.up.railway.app/log"
+API_URL = "http://127.0.0.1:8000/logs"
 API_KEY = "secret123"
 
 HEADERS = {
